afe472ab89fc1d113b30d69d273a281da7c1bf9a
Rubén Baltasar da Cuña González <a24rubenbdcg@teis.intranet>
2026-04-14 21:10:10 +0200

    Test 2

--- test_funciones.py
+++ test_funciones.py
@@ -6,6 +6,9 @@ def test_suma():
 def test_es_palindromo():
     assert es_palindromo("ana") is True
     assert es_palindromo("hola") is False
+    assert es_palindromo("Radar") is True
+    assert es_palindromo("Coca cola") is False
+
 
 def test_misma_longitud():
     assert misma_longitud("hola","cola") is True
@@ -14,11 +17,15 @@ def test_misma_longitud():
 def test_son_anagramas():
     assert son_anagramas("hola","cola") is False
     assert son_anagramas("roma","amor") is True
+    assert son_anagramas("jamon", "monja") is True
 
 def test_inicia_con_mayuscula():
     assert inicia_con_mayuscula("Hola") is True
     assert inicia_con_mayuscula("casa") is False
+    assert inicia_con_mayuscula("jamoN") is False
+    assert inicia_con_mayuscula("SerpientE") is False
 
 def test_contiene_espacios():
     assert contiene_espacios("hola") is False
     assert contiene_espacios("Coca Cola") is True
+    assert contiene_espacios("La ruta natural") is True
